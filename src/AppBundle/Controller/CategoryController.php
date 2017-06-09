67f0de598da94d76a7a8fce49322b0f8c8ecff3f
Alex <allapkov@gmail.com>
2017-06-09 04:13:27 +0300

    Add edit categories controller

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -2,9 +2,23 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
+    /**
+     * @param Request $request
+     * @Route("/categories")
+     * @return Response
+     *
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function indexAction(Request $request)
+    {
+        return new Response('Table of categories');
+    }
 }
