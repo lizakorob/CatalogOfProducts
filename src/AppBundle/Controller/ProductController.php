3f234d7f72653373ae4a97e5b3698e9dc13a20de
Alex <allapkov@gmail.com>
2017-06-09 21:37:17 +0300

    Add controller of product

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -2,9 +2,29 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * Class ProductController
+ * @package AppBundle\Controller
+ * @Route("/products")
+ * @Security("has_role('ROLE_MODERATOR')")
+ */
 class ProductController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/", name="products")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $products = $em->getRepository('AppBundle:Product')->findAll();
+
+        return $this->render('products/index.html.twig', array(
+            'products' => $products,
+        ));
+    }
 }
