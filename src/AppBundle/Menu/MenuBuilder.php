915ef365f772d1cb4736d4ad4fa8f7fea575a80f
lizakorob <lizakoshak@gmail.com>
2017-06-13 16:34:34 +0300

    everything is perfect

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -33,6 +33,9 @@ class MenuBuilder
         $category1 = new Category(1, 'КОНФЕТЫ', null, 'sweets');
         $category2 = new Category(2, 'ПЕЧЕНЬЕ', null, 'cookies');
         $category3 = new Category(3, 'ПОДАРКИ', null, 'presents');
+        $category22 = new Category(31, 'ЕВРОПЕЙСКИЕ СЛАДОСТИ', null, 'presents');
+        $category223 = new Category(32, 'ЕЩЕ2', null, 'presents');
+        $category32 = new Category(333, 'ЕЩЕ3', null, 'presents');
         $category4 = new Category(4, 'Леденцы', 1, '123');
         $category5 = new Category(5, 'Шоколадные', 1, '123');
         $category6 = new Category(6, 'Суфле', 1, '123');
@@ -43,14 +46,15 @@ class MenuBuilder
         $category11 = new Category(11, 'На новый год', 3, '123');
         $cat = new Category(100, 'fbfdfbfd', 4, '111');
         $array = [$category1, $category2, $cat, $category3, $category4, $category5, $category6,
-            $category7, $category8, $category8, $category9, $category10, $category11];
+            $category7, $category8, $category8, $category9, $category10, $category11, $category22, $category32, $category223];
 
         $menu = $this->factory->createItem('main');
-//        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav mainMenu col-md-12');
         foreach ($array as $category) {
             if ($category->getParent() == null) {
-                $item = $menu->addChild($category->getName(), array('route' => 'login'/*'catalog/'.$category->getUrl()*/))
-                    ->setAttribute('class', 'dropdown')
+                $item = $menu->addChild($category->getName(), array('route' => 'login',/*'catalog/'.$category->getUrl()*/))
+                    ->setAttribute('class', 'dropdown col-xs-6 col-sm-2')
+                    ->setAttribute('icon', 'fa fa-home')
                     ->setLinkAttribute('data-toggle', 'dropdown')
                     ->setLinkAttribute('class', 'dropdown-toggle disabled')
                     ->setChildrenAttribute('class', 'dropdown-menu')
@@ -82,7 +86,7 @@ class MenuBuilder
 
         $menu = $this->factory->createItem('sidebar');
 //        if (isset($options['homepage']) && $options['homepage']) {
-
+        $menu->setChildrenAttribute('class', 'hidden-xs hidden-sm');
         foreach ($array as $category) {
             if ($category->getParent() == null) {
                 $item = $menu->addChild( $category->getName(), array('route' => 'login'))
